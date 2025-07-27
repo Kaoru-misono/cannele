c76d1b469633a448d5dbc2c58c7b9f502d271ab0
Kaoru-misono <22031037@zju.edu.cn>
2025-07-27 20:59:50 +0800

    Add camera.

---
+++ engine/source/scene/camera.cpp
@@ -0,0 +1,201 @@
+#include "camera.hpp"
+
+#include <math/tool.hpp>
+
+namespace cannele::inline scene
+{
+    Camera::Camera()
+    {}
+
+    auto Camera::set_orthogonal(float near_z, float far_z, float width, float height, float zoom) -> void
+    {
+        mode = Projection::orthogonal;
+        this->near_z = near_z;
+        this->far_z = far_z;
+        viewport_width = width;
+        viewport_height = height;
+        this->zoom = zoom;
+        reset_state();
+
+        is_projection_dirty = true;
+    }
+
+    auto Camera::set_perspective(float near_z, float far_z, float fov, float aspect_ratio) -> void
+    {
+        mode = Projection::perspective;
+        this->near_z = near_z;
+        this->far_z = far_z;
+        this->fov = fov;
+        this->aspect_ratio = aspect_ratio;
+        reset_state();
+
+        is_projection_dirty = true;
+    }
+
+    auto Camera::set_near_far_plane(float near_z, float far_z) -> void
+    {
+        this->near_z = near_z;
+        this->far_z = far_z;
+
+        is_projection_dirty = true;
+    }
+
+    auto Camera::set_position(math::float3 const& position) -> void
+    {
+        this->position = position;
+
+        is_view_dirty = true;
+    }
+
+    auto Camera::set_direction(math::float3 const& direction) -> void
+    {
+        auto pitch_yaw = direction_to_pitch_yaw(direction);
+        // pitch = pitch_yaw.x;
+        // yaw = pitch_yaw.y;
+
+        is_view_dirty = true;
+    }
+
+    auto Camera::set_viewport(float width, float height) -> void
+    {
+        viewport_width = width;
+        viewport_height = height;
+
+        is_projection_dirty = true;
+    }
+
+    auto Camera::set_zoom(float zoom) -> void
+    {
+        this->zoom = zoom;
+
+        is_projection_dirty = true;
+    }
+
+    auto Camera::set_fov(float fov) -> void
+    {
+        this->fov = fov;
+
+        is_projection_dirty = true;
+    }
+
+    auto Camera::set_aspect_ratio(float aspect_ratio) -> void
+    {
+        this->aspect_ratio = aspect_ratio;
+
+        is_projection_dirty = true;
+    }
+
+    auto Camera::rotate(float delta_pitch, float delta_yaw) -> void
+    {
+        pitch += delta_pitch;
+        yaw += delta_yaw;
+
+        if (pitch > constraint_pitch) pitch = constraint_pitch;
+        if (pitch < -constraint_pitch) pitch = -constraint_pitch;
+
+        is_view_dirty = true;
+    }
+
+    auto Camera::update() -> void
+    {
+        if (!is_view_dirty && !is_projection_dirty) return;
+
+        if (is_view_dirty) update_view();
+        if (is_projection_dirty) update_projection();
+
+        matrixes.matrix_proj_view = matrixes.matrix_proj * matrixes.matrix_view;
+        matrixes.matrix_inv_proj_view = glm::inverse(matrixes.matrix_proj_view);
+    };
+
+    auto Camera::reset_state() -> void
+    {
+        position = math::float3{0.0f};
+        pitch = 0.0f;
+        yaw = -math::pi;
+    }
+
+    auto Camera::direction_to_pitch_yaw(math::float3 direction) -> math::float2
+    {
+        auto pitch = glm::degrees(std::asin(direction.y));
+        auto yaw = glm::degrees(std::atan2(direction.z, direction.x));
+
+        return {pitch, yaw};
+    }
+
+    auto Camera::update_view() -> void
+    {
+        forward.x = std::cos(yaw) * std::cos(pitch);
+        forward.y = std::sin(pitch);
+        forward.z = std::sin(yaw) * std::cos(pitch);
+        forward = glm::normalize(forward);
+
+        right = glm::normalize(glm::cross(forward, math::float3{0.0f, 1.0f, 0.0f}));
+        up = glm::normalize(glm::cross(right, forward));
+
+        matrixes.matrix_view = glm::lookAt(position, position + forward, math::float3{0.0f, 1.0f, 0.0f});
+        matrixes.matrix_inv_view = glm::inverse(matrixes.matrix_view);
+    }
+
+    auto Camera::update_projection() -> void
+    {
+        if (mode == Projection::orthogonal) {
+            matrixes.matrix_proj = glm::ortho(zoom * -viewport_width / 2.0f, zoom * viewport_width / 2.0f, zoom * -viewport_height / 2.0f, zoom * viewport_height / 2.0f, near_z, far_z);
+        }
+        else if (mode == Projection::perspective) {
+            matrixes.matrix_proj = glm::perspective(glm::radians(fov), aspect_ratio, near_z, far_z);
+        }
+        matrixes.matrix_inv_proj = glm::inverse(matrixes.matrix_proj);
+    }
+
+    auto First_Person_Camera::init(float rotation_speed, float move_speed, float movement_delta) -> void
+    {
+        target_yaw = yaw;
+        target_pitch = pitch;
+        target_movement = position;
+        mouse_sensitivity = 1.0f;
+
+        this->rotation_speed = rotation_speed;
+        this->movement_speed = move_speed;
+        this->movement_delta = movement_delta;
+    }
+
+    auto First_Person_Camera::update(platform::InputEvent* input, math::float2 window_size, float delta_time) -> void
+    {
+        Camera::update();
+
+        if (input->is_mouse_dragging(Mouse::right)) {
+            target_yaw += (input->mouse_position.x - input->pre_mouse_position.x) * mouse_sensitivity * delta_time;
+            target_pitch -= (input->mouse_position.y - input->pre_mouse_position.y) * mouse_sensitivity * delta_time;
+        }
+
+        auto camera_movement = math::float3{0.0f};
+
+        if (input->is_key_down(Keyboard::a)) {
+            camera_movement -= movement_delta * right;
+        }
+        else if (input->is_key_down(Keyboard::d)) {
+            camera_movement += movement_delta * right;
+        }
+
+        if (input->is_key_down(Keyboard::w)) {
+            camera_movement += movement_delta * forward;
+        }
+        else if (input->is_key_down(Keyboard::s)) {
+            camera_movement -= movement_delta * forward;
+        }
+
+        if (input->is_key_down(Keyboard::q)) {
+            camera_movement += movement_delta * up;
+        }
+        else if (input->is_key_down(Keyboard::e)) {
+            camera_movement -= movement_delta * up;
+        }
+
+        auto tween_speed = rotation_speed * delta_time;
+        Camera::rotate((target_pitch - pitch) * tween_speed, (target_yaw - yaw) * tween_speed);
+
+        target_movement += camera_movement;
+        auto tween_move_speed = movement_speed * delta_time;
+        position = math::lerp_float3(position, target_movement, 0.9f, tween_move_speed);
+    }
+}
---
+++ engine/source/scene/camera.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <math/type.hpp>
+#include <platform/input/input.hpp>
+
+namespace cannele::inline scene
+{
+    struct Camera_Matrix final
+    {
+        math::float4x4 matrix_view{};
+        math::float4x4 matrix_inv_view{};
+        math::float4x4 matrix_proj{};
+        math::float4x4 matrix_inv_proj{};
+        math::float4x4 matrix_proj_view{};
+        math::float4x4 matrix_inv_proj_view{};
+    };
+
+    struct Camera
+    {
+        enum struct Projection: uint8_t
+        {
+            perspective,
+            orthogonal,
+        };
+
+        Camera();
+        virtual ~Camera() = default;
+
+        auto set_orthogonal(float in_near_z, float in_far_z, float in_width, float in_height, float in_zoom) -> void;
+        auto set_perspective(float in_near_z, float in_far_z, float in_fov, float in_aspect_ratio) -> void;
+
+        auto set_near_far_plane(float in_near_z, float in_far_z) -> void;
+        auto set_position(math::float3 const& in_position) -> void;
+        auto set_direction(math::float3 const& in_direction) -> void;
+
+        // Orthogonal.
+        auto set_viewport(float in_width, float in_height) -> void;
+        auto set_zoom(float in_zoom) -> void;
+
+        // Perspective.
+        auto set_fov(float in_fov) -> void;
+        auto set_aspect_ratio(float in_aspect_ratio) -> void;
+
+        auto rotate(float delta_pitch, float delta_yaw) -> void;
+
+        auto update() -> void;
+
+        auto matrix() -> Camera_Matrix const* { update(); return &matrixes; }
+
+        float near_z{0.01f};
+        float far_z{1000.0f};
+
+        float fov{60.0f};
+        float aspect_ratio{1.0f};
+
+    protected:
+
+        math::float3 position{0.0f};
+        math::float3 forward{0.0f};
+        math::float3 right{0.0f};
+        math::float3 up{0.0f};
+
+        float pitch{0.0f};
+        float yaw{0.0f};
+
+        static constexpr float constraint_pitch{math::pi / 2.0f - 0.01f};
+
+        float zoom{0.0f};
+        float viewport_width{0.0f};
+        float viewport_height{0.0f};
+
+        Projection mode{Projection::perspective};
+
+        Camera_Matrix matrixes{};
+
+        bool is_view_dirty{true};
+        bool is_projection_dirty{true};
+
+        auto reset_state() -> void;
+        auto direction_to_pitch_yaw(math::float3 direction) -> math::float2;
+        auto update_view() -> void;
+        auto update_projection() -> void;
+    };
+
+    struct First_Person_Camera: Camera
+    {
+        First_Person_Camera() = default;
+        ~First_Person_Camera() = default;
+
+        auto init(float in_rotation_speed = 10.0f, float in_move_speed = 10.0f, float in_movement_delta = 0.05f) -> void;
+
+        auto update(platform::InputEvent* input, math::float2 window_size, float delta_time) -> void;
+
+        float movement_speed{};
+        float rotation_speed{};
+
+    private:
+
+        float mouse_sensitivity{};
+        float movement_delta{};
+
+        float target_yaw{};
+        float target_pitch{};
+        math::float3 target_movement{};
+    };
+}
